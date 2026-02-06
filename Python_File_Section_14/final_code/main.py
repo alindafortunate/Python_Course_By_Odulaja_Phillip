a9f7d36b49292aa11acf0c623d19af9c6c91ef41
alindafortunate <alindafortunate5@gmail.com>
2026-02-06 10:00:46 +0300

    Used the write() and append() methods on python files.

--- Python_File_Section_14/final_code/main.py
+++ Python_File_Section_14/final_code/main.py
@@ -1,3 +1,13 @@
 # 
-with open('final_code/intro.txt', 'r') as file:
+with open('intro.txt', 'r') as file:
     print(file.read())
+
+#Using the write() method
+with open('intro.txt', 'w') as new_file :
+    new_file.write('I will not only be developing softwares but also tutoring software development concepts.') 
+    # The above method replaces whatever was in the file.
+
+#Using the append() method.
+#Remember we use this method together with the write() to add the desired contents to the file.
+with open('intro.txt','a') as updated_file:
+    updated_file.write('\nI am Alinda Fortunate \nA Software Developer \nI am 28 years.')
