7db485a25aa63e494256792bac6516c9700dd518
alindafortunate <alindafortunate5@gmail.com>
2026-02-06 10:24:26 +0300

    Used the readline() and readlines() methods on python files.

--- Python_File_Section_14/final_code/main.py
+++ Python_File_Section_14/final_code/main.py
@@ -1,13 +1,23 @@
-# 
-with open('intro.txt', 'r') as file:
-    print(file.read())
+# # 
+# with open('intro.txt', 'r') as file:
+#     print(file.read())
 
-#Using the write() method
-with open('intro.txt', 'w') as new_file :
-    new_file.write('I will not only be developing softwares but also tutoring software development concepts.') 
-    # The above method replaces whatever was in the file.
+# #Using the write() method
+# with open('intro.txt', 'w') as new_file :
+#     new_file.write('I will not only be developing softwares but also tutoring software development concepts.') 
+#     # The above method replaces whatever was in the file.
 
-#Using the append() method.
-#Remember we use this method together with the write() to add the desired contents to the file.
-with open('intro.txt','a') as updated_file:
-    updated_file.write('\nI am Alinda Fortunate \nA Software Developer \nI am 28 years.')
+# #Using the append() method.
+# #Remember we use this method together with the write() to add the desired contents to the file.
+# with open('intro.txt','a') as updated_file:
+#     updated_file.write('\nI am Alinda Fortunate \nA Software Developer \nI am 28 years.')
+
+# Exploring the readline() method
+with open('intro.txt', 'r') as new_file:
+    print(new_file.readline())
+
+#Exploring readlines()
+# This method creates a list of items take the items line by line.
+with open('intro.txt', 'r') as new1_file:
+    print(new1_file.readlines()) 
+    
