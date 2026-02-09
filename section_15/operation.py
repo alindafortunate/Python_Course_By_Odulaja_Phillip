2de87438aafa3858ed816cf0d7d0562b43609500
alindafortunate <alindafortunate5@gmail.com>
2026-02-09 17:59:38 +0300

    Higher order function with a lambda function.

---
+++ section_15/operation.py
@@ -0,0 +1,7 @@
+# passing a lambda function inside another function.
+def operation(operation, a, b):
+    return operation(a, b)
+
+
+multiply = lambda x, y: x * y
+print(operation(multiply, 3, 5))
