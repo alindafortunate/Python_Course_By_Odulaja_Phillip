0d029277ace6f5bf6db802a867b0e068ef307123
alindafortunate <alindafortunate5@gmail.com>
2026-02-11 18:14:02 +0300

    Understood more about pattern matching using different helpers.

--- section_15/searching_for_patterns.py
+++ section_15/searching_for_patterns.py
@@ -4,8 +4,13 @@ import re
 
 with open(r"text.txt", "r") as text:
     content = text.read()
-    pattern = re.compile(r"\D\d\D\D\d\d\d\D\D\d")
-    matches = re.finditer(pattern, content)
+    # pattern = re.compile(r"\D\d\D\D\d\d\d\D\D\d") # Digits and None digits.
+    # word boundary
+    # pattern = re.compile(r"\bhello")
+
+    # matching usernames.
+    pattern = re.compile(r"\B@\w\w\w\w\w\w\w")
+    matches = pattern.finditer(content)
 
     for match in matches:
         print(match)
