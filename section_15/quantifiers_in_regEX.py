ee724aa065bdbf189a9f9bb34858fb6075afcd74
alindafortunate <alindafortunate5@gmail.com>
2026-02-12 11:39:50 +0300

    Matched urls with a minimal error about the literal dot.

--- section_15/quantifiers_in_regEX.py
+++ section_15/quantifiers_in_regEX.py
@@ -3,8 +3,11 @@ import re
 
 with open(r"text.txt", "r") as text_file:
     content = text_file.read()
-    # pattern to match usernames easily
-    pattern = re.compile(r"\B@\w+")
+    # matching usernames
+    # pattern = re.compile(r"\B@\w+")
+
+    # matching urls
+    pattern = re.compile(r"https?://(www.)?\w+.com")
     matches = pattern.finditer(content)
     for match in matches:
         print(match)
