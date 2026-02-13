4f3d0f7069d430439f50a6ae9543f2105d9a0e74
alindafortunate <alindafortunate5@gmail.com>
2026-02-13 19:42:58 +0300

    Worked with list comprehension.

---
+++ section_16/list_comprehension1.py
@@ -0,0 +1,9 @@
+# Working with list comprehesion.
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# To get the even numbers of the numbers using list comprehension.
+even_numbers = [n for n in numbers if n % 2 == 0]
+
+print(numbers)
+print(even_numbers)
