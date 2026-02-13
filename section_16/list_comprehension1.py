1c6f4bb5067338270fbf7459ceda0e3df98cbfbc
alindafortunate <alindafortunate5@gmail.com>
2026-02-13 19:49:29 +0300

    Understanding list comprehension more.

--- section_16/list_comprehension1.py
+++ section_16/list_comprehension1.py
@@ -5,5 +5,8 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 # To get the even numbers of the numbers using list comprehension.
 even_numbers = [n for n in numbers if n % 2 == 0]
 
+odd_numbers_from_3 = [n for n in numbers if n % 2 != 0 and n > 1]
+
 print(numbers)
 print(even_numbers)
+print(odd_numbers_from_3)
