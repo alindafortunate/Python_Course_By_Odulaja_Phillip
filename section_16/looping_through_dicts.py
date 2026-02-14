13846b29476946605d91e976e4d903e04beef736
alindafortunate <alindafortunate5@gmail.com>
2026-02-14 12:32:54 +0300

    Worked with items() function of dictionaries.

--- section_16/looping_through_dicts.py
+++ section_16/looping_through_dicts.py
@@ -2,11 +2,23 @@
 prices = {"apple": 0.5, "banana": 0.3, "orange": 0.8}
 
 # Normal dict looping
-for fruit in prices:
-    print(fruit)  # Output is the dict keys e.g apple, banana. orange.
+# for fruit in prices:
+#     print(fruit)  # Output is the dict keys e.g apple, banana. orange.
 
 
 # If you want both keys and values, you can use:
 # .items() – returns key-value pairs as tuples
 # .values() – returns values
 # .keys() – returns keys (same as default iteration)
+
+# for items() – returns key-value pairs as tuples
+price_dict = {}
+for item in prices.items():
+    fruit = item[0]
+    price = item[1]
+    price_dict[fruit] = price
+
+print(price_dict)
+
+for fruit, price in prices.items():
+    print(fruit, price)
