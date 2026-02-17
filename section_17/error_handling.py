97c70d3a90937bf5883ce61d252d0f9de179d1c1
alindafortunate <alindafortunate5@gmail.com>
2026-02-17 12:11:20 +0300

    Worked with error handling in Python.

---
+++ section_17/error_handling.py
@@ -0,0 +1,14 @@
+# try:
+#     with open(r"zsample_file.txt", 'r') as file:
+#         file.read()
+# except : # Learn't a trick that if you don't know the exact error to catch better put nothing like this.
+#     print("File doesn't exist")
+
+
+try:
+    with open(r"zsample_file.txt", 'r') as file:
+        print(file.read())
+except FileNotFoundError: 
+    with open(r'sample.txt', 'w') as new_file:
+        new_file.write('I am new text here -> updated now.')
+    
