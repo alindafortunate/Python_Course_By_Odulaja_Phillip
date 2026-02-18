ddc6a5d1af0971b2d544291d908e2e5c4928783f
alindafortunate <alindafortunate5@gmail.com>
2026-02-18 18:14:39 +0300

    Used json crack extension to manipulate json data and get data.

--- section_17/complex_json.py
+++ section_17/complex_json.py
@@ -3,4 +3,8 @@ import json
 
 with open(r"complex_json_data.json", "r") as json_data:
     data = json.load(json_data)
-    print(data)
+    # print(data)
+
+    # iphone = data["store"]["departments"][0]['categories'][-1]['products'][0]
+    nike_t_shirt = data['store']['departments'][1]['categories'][0]['products'][0]
+    print(nike_t_shirt)
