7a9abceacc7b6cd6a43c6ad3267e48116a27456b
alindafortunate <alindafortunate5@gmail.com>
2026-02-18 18:20:16 +0300

    Amazing our Lord is great indeed, I am progressing.

--- section_17/complex_json.py
+++ section_17/complex_json.py
@@ -6,5 +6,5 @@ with open(r"complex_json_data.json", "r") as json_data:
     # print(data)
 
     # iphone = data["store"]["departments"][0]['categories'][-1]['products'][0]
-    nike_t_shirt = data['store']['departments'][1]['categories'][0]['products'][0]
+    nike_t_shirt = data["store"]["departments"][1]["categories"][0]["products"][0]
     print(nike_t_shirt)
