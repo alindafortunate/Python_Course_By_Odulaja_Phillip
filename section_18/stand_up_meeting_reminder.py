1e88eaaf82238189d1e0187929d3c10fe2146f04
alindafortunate <alindafortunate5@gmail.com>
2026-02-21 13:27:44 +0300

    Scheduled an email and it was sent at a specific time.

--- section_18/stand_up_meeting_reminder.py
+++ section_18/stand_up_meeting_reminder.py
@@ -10,7 +10,11 @@ import os
 import smtplib
 from smtplib import SMTP_SSL
 from email.message import EmailMessage
+from datetime import datetime
+from datetime import time
 from dotenv import load_dotenv
+import schedule
+import time
 
 load_dotenv()
 
@@ -18,23 +22,35 @@ EMAIL_ADDRESS = os.environ.get("EMAIL_ADDRESS")
 OTHER_ADDRESS = os.environ.get("OTHER_ADDRESS")
 PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
-msg = EmailMessage()
-msg["From"] = EMAIL_ADDRESS
-msg["TO"] = [EMAIL_ADDRESS, OTHER_ADDRESS]
-msg["Subject"] = "Code Review Meeting."
-msg.set_content(
-    "Receive warm greetings dear Team, \nAs earlier communicated, we shall have our standup meeting on Saturday at 9:00am.\nAttend in person."
-)
 
-# Adding an attachment.
-with open(r"meeting_letter.txt", "rb") as letter:
-    meeting_letter = letter.read()
-    letter_name = letter.name
-msg.add_attachment(
-    meeting_letter, maintype="application", subtype="octet-stream", filename=letter_name
-)
+def send_email():
+    msg = EmailMessage()
+    msg["From"] = EMAIL_ADDRESS
+    msg["TO"] = [EMAIL_ADDRESS, OTHER_ADDRESS]
+    msg["Subject"] = "Code Review Meeting."
+    msg.set_content(
+        "Receive warm greetings dear Team, \nAs earlier communicated, we shall have our standup meeting on Saturday at 9:00am.\nAttend in person."
+    )
 
-with SMTP_SSL("smtp.gmail.com", 465) as server:
-    server.login(EMAIL_ADDRESS, PASSWORD)
-    server.send_message(msg)
-    print("Email has been sent.")
+    # Adding an attachment.
+    with open(r"meeting_letter.txt", "rb") as letter:
+        meeting_letter = letter.read()
+        letter_name = letter.name
+        msg.add_attachment(
+            meeting_letter,
+            maintype="application",
+            subtype="octet-stream",
+            filename=letter_name,
+        )
+
+    with SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(EMAIL_ADDRESS, PASSWORD)
+        server.send_message(msg)
+        print("Email has been sent.")
+
+
+
+schedule.every().saturday.at('13:25').do(send_email)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
