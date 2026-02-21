3ee896f6a8a328fe9dbf54b309891d94a6d06036
alindafortunate <alindafortunate5@gmail.com>
2026-02-21 12:00:19 +0300

    Setup procedures to follow.

--- section_18/stand_up_meeting_reminder.py
+++ section_18/stand_up_meeting_reminder.py
@@ -1,1 +1,8 @@
 # A program to create a stand up meeting reminder.
+# The meeting starts at 9:00am
+# Steps
+# Creating an email to send to the attendees.
+# 1. The email message
+# (1.From who, 2.To who, 3.Subject, 4.The message body, 5.An attachment 6.Scheduled time.)
+# Resources
+# (The 1.smtplib library, 2.SMTP_SSL class, 3.email module, 4.datetime module, 5.datetime class.)
